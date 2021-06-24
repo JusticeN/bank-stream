0b045cabd5023cd11a8a6d663d3c19930abeefd3
justice nanhou <justice.nanhou@gmail.com>
2021-06-24 19:48:16 +0200

    set basic fontoffice application project

---
+++ frontoffice/src/main/java/com/db/frontoffice/FrontofficeApplication.java
@@ -0,0 +1,13 @@
+package com.db.frontoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontofficeApplication.class, args);
+	}
+
+}
---
+++ frontoffice/src/test/java/com/db/frontoffice/FrontofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.frontoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
